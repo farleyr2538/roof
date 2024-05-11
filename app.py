ef2a2d615e46932764f613c4167ab7bca7a38522
Rob Farley <farleyr2538@icloud.com>
2024-05-11 08:17:12 +0100

    testing without 'ql'

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ from sqlalchemy import MetaData, create_engine, MetaData, or_
 from sqlalchemy.orm import DeclarativeBase
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mwwlxvlluvoquu:1b2b550fde58c40f13f4b5e21e645d665a028038fc5f49f2420d0c426c49fc78@ec2-52-22-202-133.compute-1.amazonaws.com:5432/datr46pdevhh42'
-addition = "ql"
-old_url = os.environ.get('DATABASE_URL')
-index = 8
-url = old_url[:index] + addition + old_url[index:]
 
+# addition = "ql"
+url = os.environ.get('DATABASE_URL')
+# index = 8
+# url = old_url[:index] + addition + old_url[index:]
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 link = app.config['SQLALCHEMY_DATABASE_URI']
 
