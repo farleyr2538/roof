14e71dc094cd1e5f8d388702ab9ab5f606fff579
Rob Farley <farleyr2538@icloud.com>
2024-05-11 08:05:20 +0100

    using os

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +8,7 @@ from sqlalchemy.orm import DeclarativeBase
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mwwlxvlluvoquu:1b2b550fde58c40f13f4b5e21e645d665a028038fc5f49f2420d0c426c49fc78@ec2-52-22-202-133.compute-1.amazonaws.com:5432/datr46pdevhh42'
 addition = "ql"
-old_url = app.config['DATABASE_URL']
+old_url = os.environ.get('DATABASE_URL')
 index = 8
 url = old_url[:index] + addition + old_url[index:]
 
